432428ab370ce34ac6f02b3d0581b726c5fc1e9c
XDesignIvanx <ivandcdesign@gmail.com>
2022-06-06 15:12:04 -0400

    v.12

---
+++ src/components/reusable/Messenger/index.jsx
@@ -0,0 +1,17 @@
+import MessengerCustomerChat from "react-messenger-customer-chat";
+
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <MessengerCustomerChat
+        pageId="<PAGE_ID>"
+        appId="<APP_ID>"
+        htmlRef="<REF_STRING>"
+      />
+    </div>
+  );
+};
+
+export default index;
